784791149784b1f18e213d3bcca35c3c7f6dae5f
Leo Porto <leonardoreisporto@gmail.com>
2020-06-30 18:23:59 -0300

    small update

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,8 @@
 let certa = document.querySelector('#certa')
 let certa_play = document.querySelector('#certa_play')
+let errada1 = document.querySelector('#errada1')
+let errada2 = document.querySelector('#errada2')
+let errada3 = document.querySelector('#errada3')
 
 certa.onclick = function () {
 
@@ -14,5 +17,26 @@ certa.onclick = function () {
         window.location = 'pg3.html'
         
     }, 3000);
+    
+    
+}
+
+errada1.onclick = function () {
+
+    errada1.classList.add('errada')
+    
+}
+
+
+errada2.onclick = function () {
+
+    errada2.classList.add('errada')
+    
+}
+
+
+errada3.onclick = function () {
+
+    errada3.classList.add('errada')
     
 }
--- js/main5.js
+++ js/main5.js
@@ -1,10 +1,14 @@
 let certa = document.querySelector('#certa')
 let certa_play = document.querySelector('#certa_play')
+let errada1 = document.querySelector('#errada1')
+let errada2 = document.querySelector('#errada2')
+let errada3 = document.querySelector('#errada3')
 
 certa.onclick = function () {
 
     
     certa_play.play()
+
     certa.classList.add('certa')
 
 
@@ -13,5 +17,26 @@ certa.onclick = function () {
         window.location = 'pg6.html'
         
     }, 3000);
+    
+    
+}
+
+errada1.onclick = function () {
+
+    errada1.classList.add('errada')
+    
+}
+
+
+errada2.onclick = function () {
+
+    errada2.classList.add('errada')
+    
+}
+
+
+errada3.onclick = function () {
+
+    errada3.classList.add('errada')
     
 }
